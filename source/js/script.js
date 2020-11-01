5882ac600f564d19ef7b7384898fc583b4355b9d
Little-red-panda <daria.mart21@gmail.com>
2020-11-01 15:28:33 +0300

    Доработан скрипт

--- source/js/script.js
+++ source/js/script.js
@@ -1,40 +1,30 @@
-let button = document.querySelector(".main-nav__toggle");
+let navButton = document.querySelector(".main-nav__toggle");
 let nav = document.querySelector(".main-nav__wrapper");
 
-console.log(button);
+navButton.classList.remove("main-nav__toggle--nojs");
 
-button.classList.remove("main-nav__toggle--nojs");
-
-button.addEventListener("click", function(evt) {
+navButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     nav.classList.toggle("main-nav--closed");
 });
 
-button.addEventListener("click", function() {
-    if (button.classList.contains("main-nav__toggle--closed")) {
-        button.classList.remove("main-nav__toggle--closed");
-        button.classList.add("main-nav__toggle--opened");
+navButton.addEventListener("click", function() {
+    if (navButton.classList.contains("main-nav__toggle--closed")) {
+        navButton.classList.remove("main-nav__toggle--closed");
+        navButton.classList.add("main-nav__toggle--opened");
     } else {
-        button.classList.remove("main-nav__toggle--opened");
-        button.classList.add("main-nav__toggle--closed");
+        navButton.classList.remove("main-nav__toggle--opened");
+        navButton.classList.add("main-nav__toggle--closed");
     }
 });
 
 let popup = document.querySelector(".modal-cart");
-let order = document.querySelector(".top-item__order");
-let carts = document.querySelectorAll(".product-item__cart");
+let modalButton = document.querySelectorAll(".modal__btn");
 let overlay = document.querySelector(".modal-overlay");
 let addButton = document.querySelector(".modal-cart__button");
 
-order.addEventListener("click", function() {
-    popup.classList.add("modal-show");
-    overlay.classList.add("modal-show");
-});
-
-console.log(carts);
-
-for (let i = 0; i < carts.length; i++) {
-carts[i].addEventListener("click", function() {
+for (let i = 0; i < modalButton.length; i++) {
+    modalButton[i].addEventListener("click", function() {
     popup.classList.add("modal-show");
     overlay.classList.add("modal-show");
 });
